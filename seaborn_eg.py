10e2c20cdf4453ee10d0876d55af5105e4ea3ec0
Nakshatra Patel <patelnakshatra@outlook.com>
2024-09-23 22:16:13 +0100

    testing graph

--- seaborn_eg.py
+++ seaborn_eg.py
@@ -10,11 +10,101 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+def black_scholes_vega(s:float, t:float, k:float, r:float, sigma:float):
+    '''
+    
+
+    Parameters
+    ----------
+    s : float
+        Current price of underlying
+    t : float
+        time to maturity
+    k : float
+        strike
+    r : float
+        interest rate
+    sigma : float
+        volatility
+
+    Returns
+    -------
+    float
+        vega (dv/dsigma)
+
+    '''
+    add = (np.log(s / k) + (r + (sigma**2 / 2)) * t) / (sigma * np.sqrt(t))
+    return norm.pdf(add) * s * np.sqrt(t)
+    
+    
+    
+def black_scholes_e(s:float, t:float, k:float, r:float, sigma:float, opt:str):
+    '''
+    
+
+    Parameters
+    ----------
+    s : float
+        Current price of underlying
+    t : float
+        time to maturity
+    k : float
+        strike
+    r : float
+        interest rate
+    sigma : float
+        volatility
+    opt : string
+        'C' for call 
+        'P' for put
+
+    Returns
+    -------
+    current_value : float
+        value of option
+
+    '''
+    
+    if opt == 'C':
+        if t == 0:
+            current_value = max(0, s - k)
+
+        d1 = (np.log(s / k) + (r + sigma**2 / 2) * t) / (sigma * np.sqrt(t))
+        d2 = d1 - (sigma * np.sqrt(t))
+        current_value = (s * norm.cdf(d1)) - ((k * np.exp(-r * t)) * norm.cdf(d2))
+        
+    elif opt == 'p':
+        if t == 0:
+            current_value = max(0, k-s)
+        
+        d1 = (np.log(s / k) + (r + sigma**2 / 2) * t) / (sigma * np.sqrt(t))
+        d2 = d1 - (sigma * np.sqrt(t))
+        
+        current_value = ((k * np.exp(-r * t)) * norm.cdf(-d2)) - (s * norm.cdf(-d1))
+        
+    return current_value
+
+
+def implied_vol_Newton(P:float, s:float, t:float, k:float, r:float, opt:str, iterations:int):
+    
+    for i in range(iterations):
+        sigma = 1
+        
+        price_diff = black_scholes_e(s, t, k, r, sigma, opt) - P
+        # print((price_diff, black_scholes_vega(s, t, k, r, sigma)))
+        
+        if abs(price_diff) < 0.01:
+            break
+        
+        sigma = sigma - (price_diff / black_scholes_vega(s, t, k, r, sigma))
+    
+    return sigma
+
 # Generate data for a 3D surface plot
-x = np.linspace(-5, 5, 100)
-y = np.linspace(-5, 5, 100)
+x = np.linspace(0, 100000, 100)
+y = np.linspace(0, 365, 100)
 X, Y = np.meshgrid(x, y)
-Z = np.sin(np.sqrt(X**2 + Y**2))
+Z = implied_vol_Newton(6500, 63000, y, x, 0, 'C', 500)
 
 print(type(X))
 print(Y.shape)
